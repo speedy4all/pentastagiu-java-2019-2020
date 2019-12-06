7fa18f6256a5685483643f005890d5a8078d62c1
vsDeus <camioane112@gmail.com>
2019-12-06 13:25:33 +0200

    Goia Ciprian - Data Types Homework -Implemented first half of the homework

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,100 +1,168 @@
+import java.util.Scanner;
+
+
 public class Homework1 {
 
     public static void main(String[] args) {
-        // Primitives
-        int myPrimitive = 10;
-        System.out.println(Integer.toBinaryString(myPrimitive));
-        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
 
-        float myFloatPrimitive = 10.5f;
-        double myDoublePrimitive = 10.5f;
-        System.out.println(myFloatPrimitive);
+        int numar;
+        Scanner input = new Scanner(System.in);
 
-        char myCharPrimitive = 'c';
+        // Problem 1
+        System.out.println("Problema 1");
+        System.out.print("Scrie un numar: ");
+        numar = input.nextInt();
+        problem1(numar);
 
-        boolean myBooleanPrimitive = true;
-        boolean myFalseBooleanPrimitive = false;
-
-        // Objects
-        Integer myIntegerPrimitive = 10;
-
-        String myString = "Hello PentaStagiu!";
+        // Problem 2
+        System.out.println("Problema 2");
+        System.out.print("Scrie un numar: ");
+        numar = input.nextInt();
+        problem2(numar);
 
 
-        // Control blocks
+        //Problem 3
+        System.out.println("Problema 3");
+        System.out.print("Scrie un numar: ");
+        numar = input.nextInt();
+        problem3(numar);
+        System.out.println();
 
-        // Display parity
-        int myVar = 11;
-        if (myVar % 2 == 0) {
-            System.out.println("The variable is even!");
-        } else {
-            System.out.println("The variable is odd!");
+        // Problem 4
+        System.out.println("Problema 4");
+        int[] arr = {13, 6, 2, 8, 2, 3};
+        boolean equal = problem4(arr);
+        System.out.println("Output-ul pentru 13, 6, 2, 8, 2, 3 este " + equal );
+
+        int [] arrr = {13, 6, 2, 8, 2, 13};
+        equal = problem4(arrr);
+        System.out.println("Output-ul pentru 13, 6, 2, 8, 2, 13 este " + equal );
+        System.out.println();
+
+        // Problem 5
+        System.out.println("Problema 5");
+        int[] arr1 = {1, 2, 3, 4}, arr2 = {}, arr3 = {1, 3, 2}, arr4 = {1, 3}, arr5 = {1, 2, 4, 3};
+
+        System.out.println("Pentru [1,2,3,4] si [1,2,3,4] output-ul este: " + problem5(arr1, arr1));
+        System.out.println("Pentru [] si [] output-ul este: " + problem5(arr2, arr2));
+        System.out.println("Pentru [1,3,2] si [1,3] output-ul este: " + problem5(arr3, arr4));
+        System.out.println("Pentru [1,2,3,4] si [1,2,4,3] output-ul este: " + problem5(arr1, arr5));
+
+        //Problem 6
+        System.out.println("Problem 6");
+        int[] arr6 = {5, 6, 7, 8, 9};
+        problem6(arr1, arr6);
+        System.out.println();
+
+        //Problem 7
+        System.out.println("Problem 7");
+        int[] arr7 = {13,20,14,5,2,8};
+        problem7(arr7);
+        System.out.println();
+
+        //Problem 8
+        System.out.println("Problem 8");
+        int[] arr8 = {13,20,14,5,2,8};
+        int index = problem8(arr8, 5);
+        System.out.println("Elem 5 din [13,20,14,5,2,8] se alfa la pozitia: "+ index);
+
+
+    }
+
+    private static void problem1(int numar) {
+        int sum = 0, i;
+        for(i=0; i <= numar; i++)
+            sum += i;
+
+        System.out.println("Suma este: " + sum + "\n");
+    }
+
+    private static void problem2(int numar) {
+        int i, sum = 0;
+        float medie;
+
+        for(i=0; i<=numar; i++) {
+            sum += i;
+        }
+        medie = (float)sum / numar;
+        System.out.println("Suma este: " + sum);
+        System.out.println("Media este " + medie + "\n");
+    }
+
+    private static void problem3(int numar) {
+        int i, n1 = 0, n2 = 1, n3, sum = 0;
+        float medie;
+
+        System.out.print("Primele " + numar + " numere Fibonacci sunt: 1 ");
+        for (i=2; i<= numar; i++) {
+            n3 = n1 + n2;
+            System.out.print(n3 + " ");
+            n1 = n2;
+            n2 = n3;
+            sum += n3;
+        }
+        medie = (float)sum / numar;
+        System.out.println("\nMedia este: " + medie);
+    }
+
+    private static boolean problem4(int[] arr) {
+        return arr[0] == arr[arr.length - 1];
+    }
+
+    private static boolean problem5(int[] arr, int[] arrr) {
+        int lungime;
+        if (arr.length == arrr.length)
+            lungime = arr.length;
+        else
+            return false;
+
+        for(int i = 0; i<lungime; i++) {
+            if(arr[i] != arrr[i])
+                return false;
         }
 
-//        if (myVar % 2 != 0) {
-//            System.out.println("The variable is odd!");
-//        }
+        return true;
+    }
 
-        // Display week-days
-        byte weekDay = 50;
+    private static void problem6(int[] arr, int[] arrr) {
+        int lungime = arr.length + arrr.length;
+        int[] arrNou = new int[lungime];
+        for(int i =0 ; i<arr.length; i++)
+            arrNou[i] = arr[i];
+        for(int i = arr.length; i<lungime; i++)
+            arrNou[i] = arrr[i-arr.length];
+        System.out.println("Noul array este: ");
+        for(int i =0; i< lungime; i++)
+            System.out.print(arrNou[i] + " ");
 
-        switch (weekDay) {
-            case 1:
-                System.out.println("Mon");
-                break;
-            case 2:
-                System.out.println("Tue");
-                break;
-            case 3:
-                System.out.println("Wen");
-                break;
-            case 4:
-                System.out.println("Thu");
-                break;
-            case 5:
-                System.out.println("Fri");
-                break;
-            case 6:
-                System.out.println("Sat");
-                break;
-            case 7:
-                System.out.println("Sun");
-                break;
-            default:
-                System.out.println("Wrong number: " + weekDay);
+    }
+
+    private static void problem7(int[] arr) {
+        int min = arr[0], max = arr[1];
+        float medie=0;
+
+        for(int i = 0; i<arr.length; i++ ) {
+            if(arr[i] < min)
+                min = arr[i];
+            if(arr[i] > max)
+                max = arr[i];
+            medie += arr[i];
         }
 
+        medie /= arr.length;
 
-        // Loop blocks
+        System.out.println("Maximul este " + max);
+        System.out.println("Minimul este " + min);
+        System.out.println("Media este " + medie);
+    }
 
-        int[] ints = {1, 2, 3, 4, 5};
-        // Calculate sum of all numbers
-        int sum = ints[0] + ints[1] + ints[2] + ints[3] + ints[4];
+    private static int problem8(int[] arr, int elem) {
+        int index = 0;
+        for(int i=0; i<arr.length; i++)
+            if (arr[i] == elem)
+                index = i;
 
-        //for
-        sum = 0;
-        for (int i = 0; i < 5; i++) {
-            //code block
-            sum = sum + ints[i];
-        }
-        System.out.println("Veniturile mele sunt: " + sum);
-
-        int j = 0;
-        sum = 0;
-        while (j < ints.length) {
-            sum = sum + ints[j];
-            j = j + 1;
-//            j++;
-//            j+=1;
-        }
-        System.out.println("Veniturile dupa while: " + sum);
-
-        j = 0;
-        do {
-            sum = sum + ints[j];
-            j = j + 1;
-        } while (j < ints.length);
-        System.out.println("Veniturile dupa do-while: " + sum);
+        return index;
     }
 
 
