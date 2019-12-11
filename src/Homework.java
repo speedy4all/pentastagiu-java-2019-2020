0658bacf3888609697d0fc88673da245e242504f
CarmenCristian <57797097+CarmenCristian@users.noreply.github.com>
2019-12-11 20:03:57 +0200

    Carmen Cristian Homework

--- src/Homework.java
+++ src/Homework.java
@@ -117,9 +117,6 @@ public class Homework {
             System.out.println(ok);
         }
 
-//        Write a java program that will concatenate 2 arrays.
-//        For example Given 2 arrays [1,2,3,4] and [5,6,7,8] the output should be a new array containing [1,2,3,4,5,6,7,8]
-
     }
 }
 
