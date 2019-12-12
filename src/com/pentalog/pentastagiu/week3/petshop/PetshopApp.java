0f7f7f5ecfb6d3d028d49d94e665a824d9be24a7
Georgiana Filote <georgiana.filote.pentalog@praxedo.com>
2019-12-12 10:10:48 +0200

    week 3 live demo

---
+++ src/com/pentalog/pentastagiu/week3/petshop/PetshopApp.java
@@ -0,0 +1,11 @@
+package com.pentalog.pentastagiu.week3.petshop;
+
+public class PetshopApp {
+
+    public static void main(String[] args) {
+        Cat cat = new Cat("Miti", 2, 200, "black");
+
+        Cat nonameCat = new Cat();
+
+    }
+}
