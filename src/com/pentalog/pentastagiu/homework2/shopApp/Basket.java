93aba03d89afe56ee9821cc6b79e8f0cc419b1c5
MariusHerlea <mariusherlea@gmail.com>
2019-12-17 16:22:27 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Homework1.java

--- src/com/pentalog/pentastagiu/homework2/shopApp/Basket.java
+++ src/com/pentalog/pentastagiu/homework2/shopApp/Basket.java
@@ -4,40 +4,6 @@ public class Basket {
 	
 	private Customer customer;
     private double totalPrice;
-// [speedy4all/pentastagiu-java-2019-2020] Alexandru Baciu - OOP & Best Practices (Session 1) homework (#44)
-//    public Basket(Customer customer) {
-//        this.customer = customer;
-//    }
 //
-//    public void UpdateTotalPrice(double price)
-//    {
-//        totalPrice += price;
-//    }
-//
-//    public double GetTotalPrice()
-//    {
-//        double percent = 0;
-//        double total;
-//        switch (customer.getMembership())
-//        {
-//            case GOLD:
-//                percent = 0.2;
-//                break;
-//            case SILVER:
-//                percent = 0.1;
-//                break;
-//            case NO:
-//                percent = 0.0;
-//                break;
-//        }
-//        total = totalPrice - totalPrice*percent;
-//        return total;
-//    }
-//
-//    public void AddToBasket(Products product)
-//    {
-//        this.UpdateTotalPrice(product.getPrice());
-//    }
-
 
 }
