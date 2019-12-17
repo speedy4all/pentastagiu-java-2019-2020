9ae04c92d12b07b7708f9672ef5867f05d722ce0
MariusHerlea <mariusherlea@gmail.com>
2019-12-17 13:53:25 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Homework1.java

--- src/com/pentalog/pentastagiu/homework2/bankAccount/BankApp.java
+++ src/com/pentalog/pentastagiu/homework2/bankAccount/BankApp.java
@@ -1,3 +1,11 @@
+
+//Create a console application from the following requirements:
+//A bank account from where someone has the option to withdraw and deposit money
+//Write information to the console each time someone makes an operation on that account
+//Take into consideration any validations that have to be done since this is a debit account,
+// so user can’t withdraw more than he has into account. Display this information to inform
+// the user about why the operation failed
+
 package com.pentalog.pentastagiu.homework2.bankAccount;
 
 public class BankApp {
--- src/com/pentalog/pentastagiu/homework2/messageBoard/MessageBoardApp.java
+++ src/com/pentalog/pentastagiu/homework2/messageBoard/MessageBoardApp.java
@@ -1,3 +1,9 @@
+//Create a console application from the following requirements:
+//We have some users that can register into an app with name & email
+//Users are able to post messages on a board
+//Each message has a description and an author
+//The board can display messages from all the users
+
 package com.pentalog.pentastagiu.homework2.messageBoard;
 
 
@@ -5,7 +11,7 @@ import java.util.ArrayList;
 
 
 
-public class MessageBoard {
+public class MessageBoardApp {
 
 
 
