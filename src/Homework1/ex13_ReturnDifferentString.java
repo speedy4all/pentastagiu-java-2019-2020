a74443be344e3ca994a64097a7e4905a7e5912a5
andreyn22 <andrey_negura@yahoo.com>
2019-12-21 15:52:24 +0000

    HomeWork1_partial_ex1-4 HomeWork_final

--- src/Homework1/ex13_ReturnDifferentString.java
+++ src/Homework1/ex13_ReturnDifferentString.java
@@ -5,47 +5,47 @@ import java.util.Map;
 
 public class ex13_ReturnDifferentString {
     public static void main(String[] args) {
-        String alphabet = "abcd";
+        String alphabet = "abcdefgh";
         System.out.println(parse(alphabet));
     }
     // ex11_CountCharacterInAString letter = alphabet(0);
     //  System.out.println(result);
     public static String parse(String alphabet) {
-        Map<String, Integer> myap = new HashMap<String, Integer>();
+        Map<String, Integer> map = new HashMap<String, Integer>();
         //Adding elements to map
-        myap.put("a", 0);
-        myap.put("b", 1);
-        myap.put("c", 2);
-        myap.put("d", 3);
-        myap.put("e", 4);
-        myap.put("f", 5);
-        myap.put("g", 6);
-        myap.put("h", 7);
-        myap.put("i", 8);
-        myap.put("j", 9);
-        myap.put("k", 10);
-        myap.put("l", 11);
-        myap.put("m", 12);
-        myap.put("n", 13);
-        myap.put("o", 14);
-        myap.put("p", 15);
-        myap.put("q", 16);
-        myap.put("r", 17);
-        myap.put("s", 18);
-        myap.put("t", 19);
-        myap.put("u", 20);
-        myap.put("v", 21);
-        myap.put("w", 22);
-        myap.put("x", 23);
-        myap.put("y", 24);
-        myap.put("z", 25);
-        String z = ""+ myap.get("z");
+        map.put("a", 0);
+        map.put("b", 1);
+        map.put("c", 2);
+        map.put("d", 3);
+        map.put("e", 4);
+        map.put("f", 5);
+        map.put("g", 6);
+        map.put("h", 7);
+        map.put("i", 8);
+        map.put("j", 9);
+        map.put("k", 10);
+        map.put("l", 11);
+        map.put("m", 12);
+        map.put("n", 13);
+        map.put("o", 14);
+        map.put("p", 15);
+        map.put("q", 16);
+        map.put("r", 17);
+        map.put("s", 18);
+        map.put("t", 19);
+        map.put("u", 20);
+        map.put("v", 21);
+        map.put("w", 22);
+        map.put("x", 23);
+        map.put("y", 24);
+        map.put("z", 25);
+        String z = ""+ map.get("z");
         String result = "";
 //    Set<String> keys = map.keySet();
 //    for (String k : keys) {
 //        System.out.println("Key: " + k)
         for (int i = 0; i < alphabet.length(); i++) {
-            result += myap.get(""+alphabet.charAt(i));
+            result += map.get(""+alphabet.charAt(i));
         }
         return result;
     }
