62a9d00be827d9c691ad43052efd0f5ece7c7212
ADN <andrey_negura@yahoo.com>
2020-01-08 21:13:22 +0200

    HomeWork1_partial_ex1-4 HomeWork_final

---
+++ src/teste ADN/MyClass.java
@@ -0,0 +1,93 @@
+/*package teste;
+public class test_nebunii {
+    public static void main(String[] args) {
+        int x = 11;
+        System.out.println(x == 2 || x < 10); // returns true because 5 is greater than 3 AND 5 is less than 10
+    }
+}
+*/
+/*
+public class MyClass {
+    public static void main (String [] args) {
+        System.out.println("Hello world");
+        System.out.print("Hello world");
+
+    }
+}/
+
+public class MyClass {
+  public static void main(String[] args) {
+    char a = 65, b = 66, c = 67;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+  }
+}
+ */
+/*
+public class MyClass {
+    public static void main(String[] args) {
+        char a = 65, b = 66, c = 67;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}*/
+/*public class MyClass {
+    public static void main(String[]args){
+        int x=5;
+        int y=3;
+        System.out.println(x-y);
+    }
+}*/
+
+
+/*public class MyClass {
+    public static void main(String[] args) {
+        double x = 5;
+        x /= 3;
+        System.out.println(x);
+    }
+}*/
+/*public class MyClass {
+    public static void main (String []args) {
+        int x=5;
+        x %=3;
+    System.out.println(x);
+    }
+}*/
+// ? reason for result "2"
+
+/*public class MyClass {
+    public static void main(String [] args) {
+        double x = 5;
+        x %= 3;
+        System.out.println(x);
+    }
+}*/
+/*public class MyClass {
+    public static void main (String[]args){
+        int x=5;
+        System.out.println(!(x>3 && x <10));
+        //returns false because ! (not) is used to reverse the result
+    }
+}*/
+/*public class MyClass {
+    public static void main (String[]args){
+        int x=5;
+        System.out.println((x>3 && x <10));
+        //returns true
+    }
+}*/
+/*public class MyClass {
+    public static void main (String[]args){
+        String greeting = "Hello";
+    System.out.println(greeting);
+    }
+}*/
+/*public class MyClass{
+    public static void main (String[]args){
+        String txt ="sdfndfkjdfnvkfvnkdfv";
+        System.out.println("the length of the txt is: " + txt.length());
+    }
+}*/
