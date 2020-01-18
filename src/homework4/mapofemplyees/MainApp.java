7b3d5fe2590d3c134ffdbba03aaaff95bbedab14
MariusHerlea <mariusherlea@gmail.com>
2020-01-18 12:32:07 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Homework1.java
    toString

---
+++ src/homework4/mapofemplyees/MainApp.java
@@ -0,0 +1,85 @@
+package homework4.mapofemplyees;
+
+
+
+//Suppose you have a Map of employees, where the key is their age and the value is their name sorted in alphabetical order. Write a Java program that:
+//        Declares and initializes the map
+//        Adds values to it
+//        Add values for a key that is not yet present in the map
+//        Iterate over the map’s keySet and prints the name of the employes for that key
+//        Iterate over the map’s entrySet and displays the first person alphabetically
+
+
+import java.util.*;
+
+import java.util.Comparator;
+
+public class MainApp  {
+
+    public static void main(String[] args) {
+        Map<Integer, String> employee = new HashMap<Integer, String>();
+
+
+        employee.put(30, ("Jhon"));
+        employee.put(20, ("Traian"));
+        employee.put(10, ("Pull"));
+        employee.put(5, null);
+
+
+        String studentsWith5 = employee.get(5);
+        if (studentsWith5 == null) {
+            studentsWith5 = new String();
+        }
+        studentsWith5=("Abra");
+        employee.put(5, studentsWith5);
+
+        Set<Integer> employeeKeySet = employee.keySet();
+        System.out.println("Employee key set: " + employeeKeySet);
+
+
+
+
+
+        Set<Map.Entry<Integer, String>> entries = employee.entrySet();
+        System.out.println("Employee entry set: " + entries);
+
+
+
+        Comparator<Map.Entry<Integer, String>> valueComparator = new Comparator<Map.Entry<Integer,String>>() {
+
+
+
+
+
+            @Override
+            public int compare(Map.Entry<Integer, String> e1, Map.Entry<Integer, String> e2) {
+                String v1 = e1.getValue();
+                String v2 = e2.getValue();
+                return v1.compareTo(v2);
+            }
+        };
+
+
+        System.out.println("#########################################");
+
+        List<Map.Entry<Integer, String>> listOfEntries = new ArrayList<Map.Entry<Integer, String>>(entries);
+
+        // sorting HashMap by values using comparator
+        Collections.sort(listOfEntries, valueComparator);
+
+        LinkedHashMap<Integer, String> sortedByValue = new LinkedHashMap<Integer, String>(listOfEntries.size());
+
+        // copying entries from List to Map
+        for(Map.Entry<Integer, String> entry : listOfEntries){
+            sortedByValue.put(entry.getKey(), entry.getValue());
+        }
+
+        System.out.println("HashMap after sorting entries by values ");
+        Set<Map.Entry<Integer, String>> entrySetSortedByValue = sortedByValue.entrySet();
+
+        for(Map.Entry<Integer, String> mapping : entrySetSortedByValue){
+            System.out.println(mapping.getKey() + " ==> " + mapping.getValue());
+        }
+
+    }
+}
--- src/homework4/stringset/MainApp.java
+++ src/homework4/stringset/MainApp.java
@@ -1,39 +1,57 @@
 package homework4.stringset;
 
-import jdk.nashorn.internal.codegen.Compiler;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+
+//Suppose you have a set s containing some strings.
+//        Write a code fragment that uses an iterator to print out each element in set.
+//        Write a code fragment that uses a for to iterate over element
+//        Write a code fragment that uses the set stream() and the following methods:
+//        map to make a transformation of your choosing, filter based on a rule of your choosing, and collect,
+//        to collect all the strings separated by “,” with the prefix “This are the results: “ and sufix “ . Thank you!”
+
+
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MainApp {
 
     public static void main(String[] args) {
-        Set<String> setOfStocks = new HashSet<>();
-        setOfStocks.add("INFY");
-        setOfStocks.add("BABA");
-        setOfStocks.add("GOOG");
-        setOfStocks.add("MSFT");
-        setOfStocks.add("AMZN");
+        Set<String> s = new HashSet<>();
+        s.add("MNFY");
+        s.add("BABA");
+        s.add("GOOG");
+        s.add("MSFT");
+        s.add("AMZN");
 
 
-        Iterator<String> itr = setOfStocks.iterator(); // traversing over HashSet System.out.println("Traversing over Set using Iterator");
-         while(itr.hasNext()){
+        Iterator<String> itr = s.iterator();
+        while(itr.hasNext()){
              System.out.println(itr.next());
          }
 
 
         System.out.println("Looping over HashSet using advanced for loop");
-         for (String stock : setOfStocks) {
+         for (String stock : s) {
 
              System.out.println(stock); }
 
-        System.out.println("stream");
+        System.out.println("_________________________");
 
-        Stream<String> stringStream=setOfStocks.stream();
+        Stream<String> stringStream=s.stream();
 
-         stringStream.forEach(elem-> System.out.println(elem+" "));
+
+         stringStream.filter(e->{
+//             System.out.println("filter : " + e);
+             return e.startsWith("M");
+         })
+                 .map(elem -> {
+                     System.out.println(" map: " + elem);
+                     return elem.toLowerCase();
+                 })
+                 .collect(Collectors.joining(" , ","Prefix:", ":Suffix"));
+        System.out.println("Joined: " + stringStream);
+
 
 
     }
