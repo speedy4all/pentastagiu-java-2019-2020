4a182cfb8ae6bf367227c26292b720caafde50e7
danasimonab <danasimonabuca@gmail.com>
2020-02-01 21:56:39 +0200

    Am modificat problema 1 dupa sugestiile din code review.

--- src/homework2/problem1/BankAccount.java
+++ src/homework2/problem1/BankAccount.java
@@ -7,18 +7,26 @@ public class BankAccount {
     private int accountNumber;
 
     public BankAccount(double totalAmount){
+        if (totalAmount < 0){
+            throw new IllegalArgumentException();
+        }
         this.totalAmount = totalAmount;
-
+        System.out.println("Successfully created your account !");
     }
 
     public void deposit(double amount){
+        if(amount < 0) {
+            throw new IllegalArgumentException();
+        }
         System.out.println("The amount that was added is: " + amount);
         totalAmount += amount;
         System.out.println("The new available amount is: " + totalAmount);
     }
 
     public boolean withdraw(double withdrawAmount){
-        if(totalAmount < withdrawAmount){
+        if(withdrawAmount < 0) {
+            throw new IllegalArgumentException();
+        }else if(totalAmount < withdrawAmount){
             System.out.println("This bank account doesn't have enough funds!");
             return false;
         } else {
@@ -30,18 +38,6 @@ public class BankAccount {
 
     public double getTotalAmount() {
         return totalAmount;
-    }
-
-    public void setTotalAmount(double totalValue) {
-        this.totalAmount = totalValue;
-    }
-
-    public int getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(int accountNumber) {
-        this.accountNumber = accountNumber;
     }
 
 }
--- src/homework2/problem1/BankAccountApp.java
+++ src/homework2/problem1/BankAccountApp.java
@@ -1,15 +1,48 @@
 package homework2.problem1;
 
+import java.util.Scanner;
+
 public class BankAccountApp {
     public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount(500);
-        //Deposit money
-        bankAccount.deposit(100);
+        Scanner scanner = new Scanner(System.in);
+        BankAccount bankAccount  = new BankAccount(0);;
+        int choose = -1;
+        do{
+            System.out.println("Choose one of the following options:\n 1. Deposit\n 2. Withdraw\n 3. Exit ");
+            choose = scanner.nextInt();
+            if (choose == 1){
+                System.out.println("Enter the amount to deposit: " );
+                try{
+                    bankAccount.deposit(scanner.nextInt());
+                }catch (IllegalArgumentException ex){
+                    System.out.println("Please enter a valid amount to deposit!");
+                }
+            }
+            if (choose == 2){
+                System.out.println("Enter the amount to withdraw: " );
+                try{
+                    bankAccount.withdraw(scanner.nextInt());
+                }catch(IllegalArgumentException ex){
+                    System.out.println("Please enter a valid amount to withdraw!");
+                }
 
-        //Withdraw money
-        bankAccount.withdraw(700);
 
-        System.out.println("Available amount : " + bankAccount.getTotalAmount());
+            }
+            if (choose ==3){
+                System.out.println("Exit...");
+            }
+
+        }while (choose != 3);
+
+
+//        BankAccount bankAccount = new BankAccount(-500);
+//        //Deposit money
+//        bankAccount.deposit(100);
+//
+//        //Withdraw money
+//        bankAccount.withdraw(700);
+//
+//        System.out.println("Available amount : " + bankAccount.getTotalAmount());
 
     }
 }
