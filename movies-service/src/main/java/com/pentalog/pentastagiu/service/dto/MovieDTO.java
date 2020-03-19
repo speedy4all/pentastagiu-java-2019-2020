14707e4962e8500b2deb6c57f4eca50d41288b34
Bogdan B <speedy4all@gmail.com>
2020-03-19 23:26:41 +0200

    - fixed dto type parameter

--- movies-service/src/main/java/com/pentalog/pentastagiu/service/dto/MovieDTO.java
+++ movies-service/src/main/java/com/pentalog/pentastagiu/service/dto/MovieDTO.java
@@ -7,6 +7,8 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MovieDTO {
     private String id;
@@ -22,7 +24,7 @@ public class MovieDTO {
     @NotNull(message = "Rating can not be null!")
     private Double rating;
 
-    private Actors actors;
+    private List<ActorDTO> actors = new ArrayList<>();
 
     public MovieDTO() {
     }
@@ -40,19 +42,11 @@ public class MovieDTO {
         this.rating = rating;
     }
 
-    public MovieDTO(String id, @NotEmpty(message = "Name can not be empty!") String name, @NotEmpty(message = "URL can not be empty!") String posterUrl, @Min(value = 0, message = "Rating can not be lower than 0!") @Max(value = 10, message = "Rating can not be higher than 10") @NotNull(message = "Rating can not be null!") Double rating, Actors actors) {
+    public MovieDTO(String id, @NotEmpty(message = "Name can not be empty!") String name, @NotEmpty(message = "URL can not be empty!") String posterUrl, @Min(value = 0, message = "Rating can not be lower than 0!") @Max(value = 10, message = "Rating can not be higher than 10") @NotNull(message = "Rating can not be null!") Double rating, List<ActorDTO> actors) {
         this.id = id;
         this.name = name;
         this.posterUrl = posterUrl;
         this.rating = rating;
-        this.actors = actors;
-    }
-
-    public Actors getActors() {
-        return actors;
-    }
-
-    public void setActors(Actors actors) {
         this.actors = actors;
     }
 
@@ -82,5 +76,13 @@ public class MovieDTO {
 
     public void setPosterUrl(String posterUrl) {
         this.posterUrl = posterUrl;
+    }
+
+    public List<ActorDTO> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<ActorDTO> actors) {
+        this.actors = actors;
     }
 }
